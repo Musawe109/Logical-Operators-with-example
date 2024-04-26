0dfc0267643a86fe5663ad4cd749b279311ab93a
Musawer Khan <musawerbangash109@gmail.com>
2024-04-26 20:47:53 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict";
+//  //Logical Operators
+let IssbRecommendation = true;
+let initialResult = "Passed";
+const issbResult = "Recommended";
+if (IssbRecommendation && initialResult < issbResult) {
+    console.log("You are not Recommended!");
+}
+else {
+    console.log("Congratulations You are Recommended!");
+}
+//Another Example
+let Arman_budget = 600000;
+let haveGirl = true;
+console.log(Arman_budget >= 100000 && haveGirl == true);
+////Another Example
+let isRaining = true;
+if (isRaining) {
+    console.log("wear a rain coat");
+}
+else
+    console.log("wear a sunglasses");
+//Another Example
+const userID = true;
+let userName = "Musawer109@gmail.com";
+let password = 12345;
+if (userID) {
+    console.log("Login Successful");
+}
+else
+    console.log("Username and Password is incorrect");
---
+++ main.ts
@@ -0,0 +1,39 @@
+  //  //Logical Operators
+
+
+  let IssbRecommendation:Boolean = true;
+  let initialResult:string = "Passed";
+  const issbResult:string = "Recommended"
+  if (IssbRecommendation && initialResult < issbResult) {
+  console.log("You are not Recommended!")  
+
+  }
+  else {
+    console.log("Congratulations You are Recommended!")
+  }
+
+
+    //Another Example
+      
+  let Arman_budget = 600000;
+  let haveGirl:boolean = true;
+  console.log(Arman_budget >=100000 && haveGirl == true )   
+
+   ////Another Example
+
+    let isRaining:boolean =true;
+    if (isRaining) {
+      console.log("wear a rain coat")
+    }
+   else console.log("wear a sunglasses")
+
+
+    //Another Example
+    
+   const userID:boolean = true;
+   let userName:string = "Musawer109@gmail.com";
+   let password:number = 12345;
+   if (userID )  {
+   console.log("Login Successful")
+   }   else
+   console.log("Username and Password is incorrect")
